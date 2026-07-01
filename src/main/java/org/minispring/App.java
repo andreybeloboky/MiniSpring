dd89e9d67a58e753ea8bffe84dace84f13563fd9
Andrey_Beloboky <andrei.beloboky@yandex.ru>
2026-07-01 16:51:35 +0300

    main branch

---
+++ src/main/java/org/minispring/App.java
@@ -0,0 +1,9 @@
+package org.minispring;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    }
+}
--- src/test/java/org/minispring/AppTest.java
+++ src/test/java/org/minispring/AppTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.minispring;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
